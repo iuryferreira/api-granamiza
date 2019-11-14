c94372e5cf3f8087036386877966301ea7db3147
MagnosLima <magandroid6.1@gmail.com>
2019-11-14 17:29:28 -0300

    Implementação do modelo de Usuario.

---
+++ APIGranamiza/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIGranamiza.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Avatar { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public List<Categoria> Categorias { get; set; }
+    }
+}
