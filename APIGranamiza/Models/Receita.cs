8706d505dad75e4b94bbcaef735926f454463a7c
MagnosLima <magandroid6.1@gmail.com>
2019-11-14 21:25:06 -0300

    Implementação de modelo de receita.

---
+++ APIGranamiza/Models/Receita.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIGranamiza.Models
+{
+    public class Receita
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataRemocao { get; set; }
+        public virtual Usuario Usuario { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
