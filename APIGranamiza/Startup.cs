b70bb25f6ea1ae323dec0238e185b2f6c5e23115
iuryferreira <iury.franklinferreira@gmail.com>
2019-11-20 17:00:54 -0300

    Adicao do servico de autenticacao

--- APIGranamiza/Startup.cs
+++ APIGranamiza/Startup.cs
@@ -7,12 +7,14 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Pomelo.EntityFrameworkCore.MySql;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
 
 namespace APIGranamiza
 {
@@ -21,15 +23,31 @@ namespace APIGranamiza
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            StaticConfiguration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public static IConfiguration StaticConfiguration { get; private set; }
+
 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            var conexao = Configuration["conexao"];
-            services.AddDbContext<Contexto>(options => options.UseMySql(conexao));
+            services.AddDbContext<Contexto>(options => options.UseMySql(Configuration["conexao"]));
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x => {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration["chaveJson"])),
+                    ValidateIssuer = false,
+                    ValidateAudience = false 
+                    };
+                });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -43,6 +61,8 @@ namespace APIGranamiza
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
