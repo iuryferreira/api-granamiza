9603f40e9a4653859d5673ee2117ab80be0d8cda
iuryferreira <iury.franklinferreira@gmail.com>
2019-11-20 21:54:22 -0300

    Adicao de campo necessario

--- APIGranamiza/Migrations/20191121005311_adicao_de_campo_token.Designer.cs
+++ APIGranamiza/Migrations/20191121005311_adicao_de_campo_token.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using APIGranamiza.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace APIGranamiza.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20191121005311_adicao_de_campo_token")]
+    partial class adicao_de_campo_token
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- APIGranamiza/Models/Usuario.cs
+++ APIGranamiza/Models/Usuario.cs
@@ -10,6 +10,7 @@ namespace APIGranamiza.Models
         public string Nome { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
+        public string? Token { get; set; }
         public DateTime DataCriacao { get; set; }
         #nullable enable
         public string? Avatar { get; set; }
