080f4ffa8535d0c8b89d705a610a40886f22c2d5
iuryferreira <iury.franklinferreira@gmail.com>
2019-11-20 23:38:41 -0300

    Controller completo das despesas

--- APIGranamiza/Controllers/DespesaController.cs
+++ APIGranamiza/Controllers/DespesaController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -30,5 +31,77 @@ namespace APIGranamiza.Controllers
             .ToListAsync();
         }
 
+
+        [Authorize]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Despesa>> GetDespesa(int id)
+        {
+            var despesa = await contexto.Despesa.
+            Where(d => d.DataRemocao == null && d.Id == id).FirstOrDefaultAsync();
+
+            if (despesa != null)
+            {
+                return despesa;
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult<Despesa>> Adicionar(Despesa despesa)
+        {
+            despesa.DataCriacao = DateTime.Now;
+            contexto.Despesa.Add(despesa);
+            await contexto.SaveChangesAsync().ConfigureAwait(false);
+            return CreatedAtAction("GetDespesa", new { usuarioId = despesa.UsuarioId, id = despesa.Id }, despesa);
+        }
+
+        [Authorize]
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Usuario>> Atualizar(int id, Despesa despesa)
+        {
+            if (id == despesa.Id)
+            {
+                contexto.Entry(despesa).State = EntityState.Modified;
+                try
+                {
+                    await contexto.SaveChangesAsync().ConfigureAwait(false);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DespesaExiste(id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return NoContent();
+            }
+            return BadRequest();
+        }
+        
+        [Authorize]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Despesa>> Remover(int id)
+        {
+            Despesa despesa = await contexto.Despesa.FindAsync(id);
+            if (despesa != null)
+            {
+                despesa.DataRemocao = DateTime.Now;
+                await contexto.SaveChangesAsync().ConfigureAwait(false);
+                return despesa;
+            }
+            return NotFound();
+        }
+        private bool DespesaExiste(int id)
+        {
+            return contexto.Despesa.Any(d => d.Id == id);
+        }
     }
 }
