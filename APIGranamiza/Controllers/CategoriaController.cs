778aedef0276155af920a262e018d17d37bfdbe9
iuryferreira <iury.franklinferreira@gmail.com>
2019-11-29 09:07:56 -0300

    Alteracao do metodo getall para post

--- APIGranamiza/Controllers/CategoriaController.cs
+++ APIGranamiza/Controllers/CategoriaController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -5,6 +6,7 @@ using APIGranamiza.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
 
 namespace APIGranamiza.Controllers{
 
@@ -19,24 +21,25 @@ namespace APIGranamiza.Controllers{
         }
 
         [Authorize]
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Categoria>>> GetAll([FromBody] int usuarioId){
+        [HttpPost("buscar")]
+        public async Task<ActionResult<IEnumerable<Categoria>>> GetAll([FromBody]UsuarioId usuarioId)
+        {
             
-            return await contexto.Categoria.Where(c => c.UsuarioId == usuarioId).ToListAsync();
+            return await contexto.Categoria.Where(c => c.UsuarioId == usuarioId.Id).ToListAsync();
         }
 
         [Authorize]
-        [HttpGet("receita")]
-        public async Task<ActionResult<IEnumerable<Categoria>>> GetAllReceita([FromBody] int usuarioId)
+        [HttpGet("buscar/receita")]
+        public async Task<ActionResult<IEnumerable<Categoria>>> GetAllReceita([FromBody]UsuarioId usuarioId)
         {
-            return await contexto.Categoria.Where(c => c.IsGasto == false && c.UsuarioId == usuarioId).ToListAsync();
+            return await contexto.Categoria.Where(c => c.IsGasto == false && c.UsuarioId == usuarioId.Id).ToListAsync();
         }
         
         [Authorize]
-        [HttpGet("despesa")]
-        public async Task<ActionResult<IEnumerable<Categoria>>> GetAllDespesa([FromBody] int usuarioId)
+        [HttpGet("buscar/despesa")]
+        public async Task<ActionResult<IEnumerable<Categoria>>> GetAllDespesa([FromBody]UsuarioId usuarioId)
         {
-            return await contexto.Categoria.Where(c => c.IsGasto == true && c.UsuarioId == usuarioId).ToListAsync();
+            return await contexto.Categoria.Where(c => c.IsGasto == true && c.UsuarioId == usuarioId.Id).ToListAsync();
         }
     }
 }
---
+++ APIGranamiza/Models/UsuarioId.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIGranamiza.Models
+{
+    public class UsuarioId
+    {
+        public int Id { get; set; }
+    }
+}
